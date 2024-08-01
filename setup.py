96fc9bdb411a73f12f3186a0c2a52e38e94f0402
dnieuw <r603008@virologie-host01.erasmusmc.nl>
2024-08-01 15:29:34 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+from ampliclip import __version__
+
+setup(name='squirrel',
+    version=__version__,
+    packages=find_packages(),
+    scripts=['ampliclip/ampliclip.py'],
+    package_data={"ampliclip":["data/*"]},
+    install_requires=[
+        'biopython>=1.70',
+        'pysam>=0.20'
+    ],
+    description='Tool to softclip reads in bam files based on amplicon primers',
+    url='https://github.com/dnieuw/ampliclip',
+    author='David F. Nieuwenhuijse',
+    author_email='d.nieuwenhuijse@erasmusmc.nl',
+    license='BSD 3-Clause',
+    zip_safe=False)
